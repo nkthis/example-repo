d30f32ba36aacb5d5ed861ac5da2d6a71e10da67
VortexProjects <vortex1649@gmail.com>
2024-02-14 09:03:19 +0530

    Revert "Update"
    This reverts commit c698b6a6d5fcef6a7a08c7b83a6dd2537a258f78.

--- src/main/java/com/ananda/example/ExampleApplication.java
+++ src/main/java/com/ananda/example/ExampleApplication.java
@@ -8,8 +8,6 @@ public class ExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExampleApplication.class, args);
-		System.out.println("Yeah");
-		System.out.println("Rahulla");
 	}
 
 }
