e4b7301c4dfbd200cba74ad6c86f994fc7805683
VortexProjects <vortex1649@gmail.com>
2024-02-14 09:18:11 +0530

    merging commit from the branch

--- src/main/java/com/ananda/example/ExampleApplication.java
+++ src/main/java/com/ananda/example/ExampleApplication.java
@@ -9,6 +9,7 @@ public class ExampleApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ExampleApplication.class, args);
 		System.out.println("Changes");
+		System.out.println("Branch merged.");
 	}
 
 }
